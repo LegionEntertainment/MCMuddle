56a180c0abdc2186fa151ada1b729ffbe30e40a3
Chris <chris@infiniti.dev>
2020-09-11 09:05:49 -0500

    Changed Main to copy textures folder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 
 
 def rename_to_number(first, second):
@@ -7,14 +8,21 @@ def rename_to_number(first, second):
 
 
 def randomize_directory(path):
+    try:
+        shutil.copytree('./res/textures', './textures')
+    except:
+        print('copying')
+
+    newpath = './textures' + path
+
     x = 0
     files = []
-    for i in os.listdir(path):
+    for i in os.listdir(newpath):
         if not i.__contains__('mcmeta'):
             files.append(i)
 
     for rename in files:
-        rename_to_number(path + '/' + rename, path + '/' + str(x))
+        rename_to_number(newpath + '/' + rename, newpath + '/' + str(x))
         x += 1
 
     files_tmp = files.copy()
@@ -22,16 +30,16 @@ def randomize_directory(path):
     x = 0
     for rand in files:
         r = random.randint(0, len(files_tmp) - 1)
-        os.rename(path + '/' + str(x), path + '/' + files_tmp[r])
+        os.rename(newpath + '/' + str(x), newpath + '/' + files_tmp[r])
         files_tmp.pop(r)
 
         x += 1
 
 
 randomize_directory(
-    './textures/assets/minecraft/textures/block'
+    '/assets/minecraft/textures/block'
 )
 
 randomize_directory(
-    './textures/assets/minecraft/textures/item'
+    '/assets/minecraft/textures/item'
 )
