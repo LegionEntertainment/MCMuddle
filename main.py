c40b0ac07ee175d89fc8ff24f27c8273353cc148
Chris <chris@infiniti.dev>
2020-09-11 08:36:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def randomize_directory(path):
             files.append(i)
 
     for rename in files:
-        rename_to_number(path + '\\' + rename, path + '\\' + str(x))
+        rename_to_number(path + '/' + rename, path + '/' + str(x))
         x += 1
 
     files_tmp = files.copy()
@@ -22,7 +22,7 @@ def randomize_directory(path):
     x = 0
     for rand in files:
         r = random.randint(0, len(files_tmp) - 1)
-        os.rename(path + '\\' + str(x), path + '\\' + files_tmp[r])
+        os.rename(path + '/' + str(x), path + '/' + files_tmp[r])
         files_tmp.pop(r)
 
         x += 1
